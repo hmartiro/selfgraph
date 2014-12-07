0ff2781263ad6fb4d41bcf91252d02286d096f1b
Rhouli <ryan.houlihan90@gmail.com>
2014-12-07 00:13:41 -0800

    Added k means

---
+++ selfgraph/algorithms/k_means.py
@@ -0,0 +1,70 @@
+"""
+
+"""
+
+import csv
+import numpy as np
+import matplotlib.pyplot as plt
+
+from sklearn import metrics
+from sklearn.cluster import KMeans
+from sklearn.datasets import load_digits
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import scale
+
+from selfgraph.utils.csv import import_csv
+
+
+def import_CSV(file_name):
+
+    person, word_list, people_list, X, Y = import_csv(file_name)
+
+    return word_list, people_list, X
+
+
+def execute(data, num_features):
+    reduced_data = PCA(n_components=2).fit_transform(data)
+    kmeans = KMeans(init='k-means++', n_clusters=2, n_init=10)
+    kmeans.fit(reduced_data)
+
+    # plt.figure()
+    # plt.plot(reduced_data)
+    # plt.show()
+    plt.plot([1,2,3,4], [1,4,9,16], 'ro')
+    plt.axis([0, 6, 0, 20])
+    plt.show()
+
+    #print(kmeans.cluster_centers_)
+    # Step size of the mesh. Decrease to increase the quality of the VQ.
+    #h = .02     # point in the mesh [x_min, m_max]x[y_min, y_max].
+    #
+    # # Plot the decision boundary. For that, we will assign a color to each
+    # x_min, x_max = reduced_data[:, 0].min() + 1, reduced_data[:, 0].max() - 1
+    # y_min, y_max = reduced_data[:, 1].min() + 1, reduced_data[:, 1].max() - 1
+    # xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+    #
+    # # Obtain labels for each point in mesh. Use last trained model.
+    # Z = kmeans.predict(np.c_[xx.ravel(), yy.ravel()])
+    #
+    # # Put the result into a color plot
+    # Z = Z.reshape(xx.shape)
+    # plt.figure(1)
+    # plt.clf()
+    # plt.imshow(Z, interpolation='nearest',
+    #            extent=(xx.min(), xx.max(), yy.min(), yy.max()),
+    #            cmap=plt.cm.Paired,
+    #            aspect='auto', origin='lower')
+    #
+    # plt.plot(reduced_data[:, 0], reduced_data[:, 1], 'k.', markersize=2)
+    # # Plot the centroids as a white X
+    # centroids = kmeans.cluster_centers_
+    # plt.scatter(centroids[:, 0], centroids[:, 1],
+    #             marker='x', s=169, linewidths=3,
+    #             color='w', zorder=10)
+    # plt.title('K-means clustering on the digits dataset (PCA-reduced data)\n'
+    #           'Centroids are marked with white cross')
+    # plt.xlim(x_min, x_max)
+    # plt.ylim(y_min, y_max)
+    # plt.xticks(())
+    # plt.yticks(())
+    # plt.show()
